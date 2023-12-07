1e52593bd82043ebca039ea70584513026adc694
Elbasri Abdennacer <devnasser@gmail.com>
2023-12-07 14:57:57 +0100

    stopWords

--- Algorithm/TDs/stopWords.py
+++ Algorithm/TDs/stopWords.py
@@ -1,8 +1,8 @@
 def rmARTL(inText):
-    articles_to_remove = ["de", "le", "la", "des", "les", "du", "au", "aux"]
+    wordsToRM = ["de", "le", "la", "des", "les", "du", "au", "aux"]
 
     words = inText.split()
-    filtered_words = [word for word in words if word.lower() not in articles_to_remove]
+    filtered_words = [word for word in words if word.lower() not in wordsToRM]
     result_string = ' '.join(filtered_words)
 
     return result_string
