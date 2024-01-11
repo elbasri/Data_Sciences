f853814b2ae373cd94e7063d34c5955a421fdf71
عبدالناصر البصري <devnasser@gmail.com>
2024-01-11 13:52:14 +0100

    Update conf.py

--- DB/MSSQL/conf.py
+++ DB/MSSQL/conf.py
@@ -2,5 +2,5 @@
 server = '94.130.23.31,1434'
 database = 'VolAvion'
 username = 'sa'
-password = 'NCR123@456'
+password = 'NCR123@451'
 conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
